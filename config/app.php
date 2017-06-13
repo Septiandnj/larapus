d1401aa5f7424ac38e6c6eb9d49bf25edde038c7
151610191-smkassalaambandung <151610191@smkassalaambandung.sch.id>
2017-06-13 16:02:34 +0700

    satu

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Larapus',
 
     /*
     |--------------------------------------------------------------------------
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Collective\Html\HtmlServiceProvider::class,
         //
 
         /*
@@ -224,6 +224,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'html' => Collective\Html\HtmlFacade::class,
+
 
     ],
 
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,1 @@
+<h1>Hellow Selamat Datang</h1>
